333bb08ed0e70d93382f3e8da88fdcb092f7520f
vishaldhayarkar2002 <vishaldhayarkar2002@gmail.com>
2026-09-23 16:20:14 +0530

    output added

--- Day08/demo03.js
+++ Day08/demo03.js
@@ -3,3 +3,14 @@ const math = require('./Math02')
 console.log(math)
 console.log(typeof math)
 math(10, 20)
+
+// D:\CDAC\WPT>node Day08\demo02.js
+// {
+//   add: [Function: add],
+//   substract: [Function: sub],
+//   mul: [Function: mul],
+//   div: [Function: div]
+// }
+// object
+// Addition - 30
+// Substraction - 15
--- Day08/demo05.js
+++ Day08/demo05.js
@@ -3,4 +3,11 @@ const math = require('./Math04')
 console.log(math)
 console.log(typeof math)
 math(10, 20)
+// here math is object of type function
 math.substract(20, 5)
+
+// D:\CDAC\WPT>node Day08\demo05.js
+// [Function: add] { substract: [Function: sub] }
+// function
+// Addition - 30
+// Substraction - 15
